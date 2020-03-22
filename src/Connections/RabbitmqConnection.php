049e1341be8f50ed59194d3839c0cf5bf6cb948f
gilgamesh-y <805655528@qq.com>
2020-03-22 23:27:32 +0800

    add: rabbitmq exchange bind

--- src/Connections/RabbitmqConnection.php
+++ src/Connections/RabbitmqConnection.php
@@ -45,44 +45,53 @@ class RabbitmqConnection
     protected $durability = false;
 
     /**
-     * Acknowledgment
+     * If this var is true ,it's mean dont't need ack
      *
      * @var boolean
      */
-    protected $ack = true;
+    protected $no_ack = false;
 
     /**
-     * The lsit of the rabbitmq queue name
+     * The list of the rabbitmq queue name
      *
      * @var array
      */
     protected $queues = [];
 
     /**
+     * The list of the rabbitmq exchange name
+     *
+     * @var array
+     */
+    protected $exchanges = [];
+
+    /**
      * The channel of the rabbitmq
      *
      * @var AMQPChannel
      */
-    protected $channel;
+    protected $channels = [];
 
     /**
      * The connection of the rabbitmq
      *
      * @var AMQPStreamConnection
      */
-    protected $connection;
+    protected $connection = [];
 
     public function __construct()
     {
         $config = App::get('config')->get('app.mq');
-        $rabbitmq_config = App::get('config')->get('rabbitmq');
         $this->host = $config['host'];
         $this->port = $config['port'];
         $this->user = $config['user'];
         $this->password = $config['password'];
         $this->durability = $config['durability'];
-        $this->ack = $config['ack'];
-        $this->queues = $rabbitmq_config['queues'];
+        $this->no_ack = $config['no_ack'];
+
+        $rabbitmq_name_config = App::get('config')->get('rabbitmq');
+        $this->queues = $rabbitmq_name_config['queues'];
+        $this->exchanges = $rabbitmq_name_config['exchanges'];
     }
 
     /**
@@ -92,11 +101,12 @@ class RabbitmqConnection
      */
     public function getConnection(): AMQPStreamConnection
     {
-        if (!isset($this->chaconnectionnnel)) {
-            $this->connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password);
+        $cid = \Swoole\Coroutine::getCid();
+        if (!isset($this->connection[$cid])) {
+            $this->connection[$cid] = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password);
         }
         
-        return $this->connection;
+        return $this->connection[$cid];
     }
 
     /**
@@ -106,13 +116,18 @@ class RabbitmqConnection
      */
     public function getChannel(): AMQPChannel
     {
-        if (!isset($this->channel)) {
-            $this->channel = $this->getConnection()->channel();
+        $cid = \Swoole\Coroutine::getCid();
+        if (!isset($this->channels[$cid])) {
+            $this->channels[$cid] = $this->getConnection()->channel();
         }
         
         
-        return $this->channel;
+        return $this->channels[$cid];
     }
+
+    /**
+     * Send msg
+     */
 
     /**
      * Send msg to queue
@@ -121,29 +136,119 @@ class RabbitmqConnection
      * @param $msg
      * @return void
      */
-    public function send(string $queue_name, $msg)
+    public function send_to_queue(string $queue_name, $msg)
     {
         $this->getChannel()->queue_declare($queue_name, false, $this->durability);
         $msg = new AMQPMessage($msg);
         $this->getChannel()->basic_publish($msg, '', $queue_name);
         $this->close();
+
+        return;
     }
 
-    public function receive()
+    /**
+     * Send msg to queue
+     *
+     * @param string $exchange_name
+     * @param mix $msg
+     * @param string $routing_key
+     * @return void
+     */
+    public function send_to_exchange(string $exchange_name, $msg, string $routing_key = '')
+    {
+        $type = isset($this->exchanges[$exchange_name]) ? $this->exchanges[$exchange_name]['type'] : 'fanout';
+        $this->getChannel()->exchange_declare($exchange_name, $type, false, false, false);
+        $msg = new AMQPMessage($msg);
+        $this->getChannel()->basic_publish($msg, $exchange_name, $routing_key);
+        $this->close();
+
+        return;
+    }
+
+    /**
+     * Receive msg from queue
+     *
+     * @return void
+     */
+    public function receive_from_queue()
     {
         foreach ($this->queues as $queue_name) {
             go(function () use ($queue_name) {
-                $this->getChannel()->queue_declare($queue_name, false, $this->durability);
-                $this->getChannel()->basic_qos(null, 1, null);
-                $this->getChannel()->basic_consume($queue_name, '', false, $this->ack, false, false, function($msg) {
-                    event(unserialize($msg->body));
-                    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+                defer(function () {
+                    $this->close();
                 });
+                $this->getChannel()->queue_declare($queue_name, false, $this->durability);
+                // Fair scheduling
+                $this->getChannel()->basic_qos(null, 1, null);
+
+                $this->basic_consume($queue_name);
                 while(count($this->getChannel()->callbacks)) {
                     $this->getChannel()->wait();
                 }
             });
         }
+    }
+
+    /**
+     * Receive msg from exchange
+     *
+     * @return void
+     */
+    public function receive_from_exchange()
+    {
+        foreach ($this->exchanges as $exchange => $config) {
+            go(function () use ($exchange, $config) {
+                defer(function () {
+                    $this->close();
+                });
+                $type = $config['type'];
+                $queues = isset($config['queues']) ? $config['queues'] : null;
+                // Create exchange and set type
+                $this->getChannel()->exchange_declare($exchange, $type, false, false, false);
+                // Binding queue、exchange and routing_key when routing_key exists and the type is not fanout
+                if (!$queues) {
+                    try {
+                        list($queue_name, ) = $this->getChannel()->queue_declare('', false, $this->durability, true, false);
+                        $this->getChannel()->queue_bind($queue_name, $exchange);
+                        $this->basic_consume($queue_name);
+                    } catch (\Throwable $th) {
+                        //throw $th;
+                    }
+                }
+                if ($queues && $type != 'fanout') {
+                    foreach ($queues as $queue_name => $routing_keys) {
+                        try {
+                            $this->getChannel()->queue_declare($queue_name, false, $this->durability, true, false);
+                            foreach ($routing_keys as $routing_key) {
+                                $this->getChannel()->queue_bind($queue_name, $exchange, $routing_key);
+                            }
+                            $this->basic_consume($queue_name);
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                        }
+                    }
+                }
+                while(count($this->getChannel()->callbacks)) {
+                    $this->getChannel()->wait();
+                }
+            });
+        }
+    }
+
+    public function basic_consume(string $queue_name)
+    {
+        $this->getChannel()->basic_consume($queue_name, '', false, $this->no_ack, false, false, function($msg) {
+            event(unserialize($msg->body));
+            if (!$this->no_ack) {
+                $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+            }
+        });
+    }
+
+    public function receive()
+    {
+        $this->receive_from_exchange();
+        $this->receive_from_queue();
     }
 
     public function close()
